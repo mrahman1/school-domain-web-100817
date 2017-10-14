a4b5af04a50f66cdcc08036cd4726d8a08fb8c28
github username <github email address>
2017-10-14 13:42:11 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,4 +1,5 @@
 
+require 'pry'
 class School
   attr_accessor :name, :roster
 
@@ -10,7 +11,7 @@ class School
   def add_student(student_name,grade)
     @roster[grade] ||= []
     @roster[grade] << student_name
-    @roster
+
   end
 
   def grade(grade)
@@ -18,7 +19,11 @@ class School
   end
 
   def sort
-    @roster.sort
+    sorted = {}
+    @roster.each do |grade, student|
+    sorted[grade] = student.sort
+    end
+    sorted
   end
 
 end
