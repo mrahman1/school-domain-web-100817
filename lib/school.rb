6f4609a49987851e09ef7a4f40b5707a8618c215
github username <github email address>
2017-10-14 13:26:18 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+
+class School
+  attr_accessor :name, :roster
+
+  def initialize(name)
+    @argument = name
+    @roster = {}
+  end
+
+  def add_student(student_name,grade)
+    @roster[grade] ||= []
+    @roster[grade] << student_name
+    @roster
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.sort
+  end
+
+end
+
+Bayside = School.new("Bayside")
+Bayside.add_student("Zach",9)
